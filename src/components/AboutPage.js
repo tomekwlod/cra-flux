ab04cf61a5b159ce2838e5300ebc9dc7adcad9e2
Tomasz Wlodarczyk <tomek.wlod@gmail.com>
2020-04-21 12:47:52 +0100

    States and props but not yet hooks

---
+++ src/components/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+    render() {
+        return (
+            <>
+                <h1>About page</h1>
+                <p>About page paragraph</p>
+            </>
+        )
+    }
+}
+
+export default AboutPage;
---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+    return (
+        <div className="jumbotron">
+            <h1>Home page</h1>
+            <p>Home page paragraph</p>
+        </div>
+    )
+}
+
+export default HomePage;
