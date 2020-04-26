322a7954abfb02fc3a0ff5e28fa943284ea5bfa1
Tomasz Wlodarczyk <tomek.wlod@gmail.com>
2020-04-26 22:58:37 +0100

    Hooks

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,42 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { getCourses } from "../api/courseApi"
+import CoursesList from  "./CoursesList";
 
-class CoursesPage extends React.Component {
-    state = {
-        courses: []
-    };
+function CoursesPage() {
     
-    componentDidMount() {
-        getCourses().then( courses => this.setState({ courses: courses }) );
-    }
+    const [ courses, setCourses ] = useState([]);
+    
+    useEffect( () => {
+        getCourses().then( _courses => setCourses(_courses) );
+    }, [] );
+    
+    return (
+        <>
+            <h2>Courses</h2>
+            <CoursesList courses={courses} />
+        </>
+    );
 
-    render() {
-        return (
-            <>
-                <h2>Courses</h2>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Author ID</th>
-                            <th>Category</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.courses.map(course => {
-                        return (
-                            <tr key={course.id}>
-                                <td>{course.title}</td>
-                                <td>{course.authorId}</td>
-                                <td>{course.category}</td>
-                            </tr>
-                        );
-                        })}
-                    </tbody>
-                </table>
-            </>
-        );
-    }
 }
 
 export default CoursesPage;
